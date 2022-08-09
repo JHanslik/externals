7cc19f1f57184623b6f672c10531a453a6a42770
JHanslik <julien.hanslik@gmail.com>
2022-08-09 11:00:34 +0200

    exercice done

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -10,10 +10,14 @@ app.get("/all", async (req, res) => {
     res.json(response.data.results);
 });
 app.get("/:id", async (req, res) => {
-    const response = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${req.params.id}`
-    );
-    res.json(response.data);
+    if (req.params.id <= 905) {
+        const response = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${req.params.id}`
+        );
+        res.json(response.data);
+    } else {
+        res.json("No pokemon here");
+    }
 });
 
 module.exports = app;
