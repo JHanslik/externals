d0a9e371b8a0ec068de876ebac790cc3a393669d
JHanslik <julien.hanslik@gmail.com>
2022-08-09 10:14:16 +0200

    exercice done

--- routes/got.js
+++ routes/got.js
@@ -1,5 +1,18 @@
-const { json } = require("express");
 const express = require(`express`);
 const app = express();
+const axios = require("axios");
+const got = require("../got.json");
+
+app.get("/json", async (req, res) => {
+    const response = got;
+    res.json(response);
+});
+
+app.get("/url", async (req, res) => {
+    const response = await axios.get(
+        `https://thronesapi.com/api/v2/Characters`
+    );
+    res.send(response.data);
+});
 
 module.exports = app;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,5 +1,19 @@
-const { json } = require("express");
 const express = require(`express`);
 const app = express();
+const axios = require("axios");
+
+app.get("/all", async (req, res) => {
+    const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/?limit=905`
+    );
+
+    res.json(response.data.results);
+});
+app.get("/:id", async (req, res) => {
+    const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${req.params.id}`
+    );
+    res.json(response.data);
+});
 
 module.exports = app;
